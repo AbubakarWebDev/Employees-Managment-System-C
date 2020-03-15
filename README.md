506ac8220febc95a3f1a78412edc9da2d1429ad8
Muhammad Abubakar <90tricks90@gmail.com>
2020-03-15 17:46:17 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# BKR-C-Projects
+This Repository Contains All My C++ Projects
+
+# Motivation
+We all love to surf the Internet. Most of the times, we come across some cool awesome websites which we tend to bookmark for future use. And I love to say that I'm no exception to this behavior.
+
+Since I have found so many such awesome websites throughout my journey in this vast ocean we call Internet, I would like to share my collection with you all. I hope you will like this cool collection of mine and don't forget to star my repo!
+
+Most of the websites are just for fun and some are very useful for specific purposes. Though I have written brief information about it in front of the link which I hope will grow your curiosity. Enjoy and do share this cool collection with your friends ðŸ™‚
+
+#Introduction to Employee Management System Using C++
+
+Employ management system using C++ is menu driven program which allows us to add, update, delete and search record of an employee working in an organization. The program employee management system store employee ID, name, post, department and salary of employee. Initially, it has no data. Thus, we have to add employee records choosing appropriate option in this program (i.e. selecting option #1).
+
+Employee Management system using C++ program uses EmpID as unique identifier (i.e. primary key) to recognize employee. So, we can’t add two employees having same ID. After successful entry of records of some employee, we can search record on the basic of ID or department. We can search particular employee from ID choosing option #2 and if we want to see all employee working in a department, we have to choose another option (i.e. option#3 in menu). We can list all employee records also using option #4 in menu. Again, we can update and delete existing record. Thus, this project is useful to mange employee records in an organization.
+
---
+++ program.cpp
@@ -0,0 +1,402 @@
+#include <iostream>
+#include <fstream>
+#include <limits>
+#include <cstring>
+#include <iomanip>
+using namespace std;
+
+bool validateInput();
+int mainmenu();
+
+struct Employee
+{
+private:
+     int id;
+     char name[100];
+     int age;
+     int salary;
+
+public:
+     // constructor function
+     Employee()
+     {
+          // Default Initialization Except Garbage Value
+          id = 0;
+          strcpy(name, "no name");
+          age = 0;
+          salary = 0;
+     }
+
+     // member function
+     void getData();
+     void writeFile();
+     void readFile();
+     void showData();
+     void searchData(char *str);
+     void updateData(char *str);
+     void deleteData(char *str);
+};
+
+void Employee::getData()
+{
+label:
+     bool flag;
+
+     cin.ignore();
+     cout << "Enter Your Name : ";
+     cin.getline(name, 100);
+
+     flag = validateInput();
+     if (flag)
+          goto label;
+
+     cout << "Enter Your ID : ";
+     cin >> id;
+
+     flag = validateInput();
+     if (flag)
+          goto label;
+
+     cout << "Enter Your Age : ";
+     cin >> age;
+
+     flag = validateInput();
+     if (flag)
+          goto label;
+
+     cout << "Enter Your Salary : ";
+     cin >> salary;
+
+     flag = validateInput();
+     if (flag)
+          goto label;
+}
+
+void Employee::writeFile()
+{
+     if (name == "no name" && id == 0)
+     {
+          cout << "Employee Record is not initalized" << endl;
+     }
+     else
+     {
+          fstream file;
+          file.open("demo.dat", ios::out | ios::app | ios::binary);
+
+          file.write((char *)this, sizeof(*this));
+
+          file.close();
+     }
+}
+
+void Employee::readFile()
+{
+     fstream file;
+     file.open("demo.dat", ios::in | ios::binary);
+
+     if (!file)
+     {
+          cout << "File can not Open";
+     }
+     else
+     {
+          if (file.peek() == ifstream::traits_type::eof())
+          {
+               cout << "\nYour File is Empty! No Record is Avialable to Show\n";
+          }
+          else
+          {
+               file.read((char *)this, sizeof(*this));
+
+               while (!file.eof())
+               {
+                    showData();
+                    file.read((char *)this, sizeof(*this));
+               }
+
+               file.close();
+          }
+     }
+}
+
+void Employee::showData()
+{
+     cout << "Enter Your Name : ";
+     cout << name << endl;
+     
+     cout << "Enter Your ID : ";
+     cout << id << endl;
+
+     cout << "Enter Your Age : ";
+     cout << age << endl;
+
+     cout << "Enter Your Salary : ";
+     cout << salary << endl;
+
+     cout << endl
+          << endl;
+}
+
+void Employee::searchData(char *str)
+{
+     bool flag = false;
+
+     fstream file;
+     file.open("demo.dat", ios::in | ios::binary);
+
+     if (!file)
+     {
+          cout << "File Cannot be Open";
+     }
+     else
+     {
+          file.read((char *)this, sizeof(*this));
+          while (!file.eof())
+          {
+               if (!strcmp(str, name))
+               {
+                    showData();
+                    flag = true;
+               }
+               file.read((char *)this, sizeof(*this));
+          }
+
+          file.close();
+
+          if (!flag)
+          {
+               cout << "Record For This Name Does Not Exist";
+          }
+     }
+}
+
+void Employee::updateData(char *str)
+{
+     bool flag = false;
+     fstream file;
+     file.open("demo.dat", ios::in | ios::out | ios::ate | ios::binary);
+
+     if (!file)
+     {
+          cout << "File Cannot Open Successfully!";
+     }
+     else
+     {
+          file.seekg(0, ios::beg);
+          file.read((char *)this, sizeof(*this));
+          while (!file.eof())
+          {
+               if (!strcmp(str, name))
+               {
+                    showData();
+                    getData();
+                    int position = (-1) * static_cast<int>(sizeof(*this));
+                    file.seekp(position, ios::cur);
+                    file.write((char *)this, sizeof(*this));
+                    flag = true;
+               }
+               file.read((char *)this, sizeof(*this));
+          }
+
+          if (!flag)
+          {
+               cout << "Record For This Name Does Not Exist";
+          }
+     }
+}
+
+void Employee::deleteData(char *str)
+{
+     int c1, c2;
+     bool flag = true;
+     ofstream fout;
+     ifstream fin;
+     fin.open("demo.dat", ios::in | ios::binary);
+
+     if (!fin)
+     {
+          cout << "File Cannot Open Successfully!";
+     }
+     else
+     {
+          fin.read((char *)this, sizeof(*this));
+          fout.open("tempfile.dat", ios::out | ios::binary);
+          while (!fin.eof())
+          {
+               if (strcmp(str, name))
+               {
+                    fout.write((char *)this, sizeof(*this));
+               }
+               fin.read((char *)this, sizeof(*this));
+          }
+          fin.close();
+          fout.close();
+
+          fstream f1, f2;
+          f1.open("demo.dat", ios::in | ios::binary);
+          f2.open("tempfile.dat", ios::in | ios::binary);
+
+          while (true)
+          {
+               c1 = f1.get();
+               c2 = f2.get();
+               if (c1 != c2)
+               {
+                    flag = false;
+                    break;
+               }
+               if ((c1 == EOF) || (c2 == EOF))
+                    break;
+          }
+
+          f1.close();
+          f2.close();
+
+          if (flag)
+          {
+               cout << "Record For This Name Does Not Exist";
+          }
+          else
+          {
+               int choice;
+               cout << "Are Your Sure to Delete This Record\n";
+               cout << "1 : Yes\n";
+               cout << "2 : No\n";
+               cout << "Enter Your Choice : ";
+               cin >> choice;
+               if (choice == 1)
+               {
+                    remove("demo.dat");
+                    rename("tempfile.dat", "demo.dat");
+                    cout << "\n\nRecord is Deleted Successfully";
+               }
+          }
+     }
+}
+
+int mainmenu()
+{
+label:
+     system("cls");
+     int choice;
+     bool flag;
+     cout << "\n\n=============== Employee Managment System ==================\n\n";
+     cout << "Please Select Your Choice :- \n";
+     cout << "1 : Insert Employees Records\n";
+     cout << "2 : View All Employees Records\n";
+     cout << "3 : Search Employees Records\n";
+     cout << "4 : Update Employee Records\n";
+     cout << "5 : Delete Employee Records\n";
+     cout << "6 : Exit Application\n";
+     cout << "Enter Your Choice : ";
+     cin >> choice;
+
+     flag = validateInput();
+     if (flag)
+     {
+          goto label;
+     }
+     else
+     {
+          return choice;
+     }
+}
+
+bool validateInput()
+{
+     if (cin.fail())
+     {
+          // Restore input stream
+          cin.clear();
+          // Clear The Previous Input
+          cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+          // Throwing Error and Again Input Value From User
+          cout << "\nError : inValid Value Detected! Please Enter Valid Value Again\n\n";
+
+          return true;
+     }
+     else
+     {
+          return false;
+     }
+}
+
+int main()
+{
+	system("color f0");
+	system("title Employeee Managment System by Abubakar");
+     bool check = true;
+     char search[100];
+     int size;
+     bool flag;
+     Employee *ptr = NULL;
+     Employee filedata;
+
+     while (check)
+     {
+          system("cls");
+          switch (mainmenu())
+          {
+          case 1:
+          label1:
+               cout << "How Many Employees Records that You Want to Store : ";
+               cin >> size;
+
+               flag = validateInput();
+
+               if (flag)
+               {
+                    goto label1;
+               }
+               else
+               {
+                    ptr = new Employee[size];
+
+                    for (int i = 0; i < size; i++)
+                    {
+                         cout << "\n\nEnter the Details For the Employee # " << i + 1 << endl;
+                         ptr[i].getData();
+                    }
+
+                    for (int i = 0; i < size; i++)
+                    {
+                         ptr[i].writeFile();
+                    }
+                    break;
+               }
+          case 2:
+               cout << "\n\n=============== Employee Details ==================\n\n";
+               filedata.readFile();
+               break;
+          case 3:
+               cout << "Please Enter Employee Name That you Want to Search its Record : ";
+               cin.ignore();
+               cin.getline(search, 100);
+               filedata.searchData(search);
+               break;
+          case 4:
+               cout << "Please Enter Employee Name That you Want to Update its Record : ";
+               cin.ignore();
+               cin.getline(search, 100);
+               filedata.updateData(search);
+               break;
+          case 5:
+               cout << "Please Enter Employee Name That you Want to Delete its Record : ";
+               cin.ignore();
+               cin.getline(search, 100);
+               filedata.deleteData(search);
+               break;
+          case 6:
+               cout << "\nThank You For Using This Application\n";
+               check = false;
+               break;
+          default:
+               cout << "Invalid Choice! Please Select Valid Choice.";
+               break;
+          }
+          cout << endl;
+          system("pause");
+     }
+}
+
